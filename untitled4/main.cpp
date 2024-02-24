9adf6fa92e2502b80ce8dea623a3d7cfd8c708e7
Alex7w7RM <90742335+Alex7w7RM@users.noreply.github.com>
2024-02-24 10:44:58 -0600

    os números del 1 al 20 en orden ascendente

--- untitled4/main.cpp
+++ untitled4/main.cpp
@@ -18,6 +18,17 @@ int main() {
 
 //Josue Alfredo Sandoval Morales 0909-22-7242
 
+    // ejercicio 2 con while
+    cout << "numero de 1 al 20 en forma ascendente" << endl;
+    int num1, e;
+    do {
+        num1 = 1 +  e;
+
+        cout << num1 << endl;
+        e++;
+    } while ( e <= 19);
+// kenny alexander torres 0909-21-1316
+
 
 
     return 0;
