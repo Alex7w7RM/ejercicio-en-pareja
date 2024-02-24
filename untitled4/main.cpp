0f6425d4c9e1c9384afb79866f8c37c2c007eb8f
Alex7w7RM <90742335+Alex7w7RM@users.noreply.github.com>
2024-02-24 10:34:22 -0600

    si un número ingresado por el usuario es positivo, negativo o cero

---
+++ untitled4/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //Ejercicio 1 con If
+        int a;
+        cout<<"Ingrese un numero: ";cin>>a;
+        if(a>0){
+            cout<<"El numero es positivo. ";
+        }
+        if(a==0){
+            cout<<"El numero es cero. ";
+        }
+        if(a<0){
+            cout<<"El numero es negativo. ";
+        }
+
+//Josue Alfredo Sandoval Morales 0909-22-7242
+
+
+
+    return 0;
+}
